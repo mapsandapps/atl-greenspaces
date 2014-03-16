fd552ed73cc8eb0c72f76954da9571a2e0bc78b9
mollietaylor <mollie.taylor@gmail.com>
2014-03-16 11:36:59 -0400

    Adds icons in legend, text to popups, splits json file

---
+++ book.js
@@ -0,0 +1,557 @@
+hikesList = {
+	"type": "FeatureCollection",
+	"features": [
+	{
+		"type": "Feature",
+		"properties": {
+			"name": "Blue Heron Nature Preserve",
+			"number": 1,
+			"distance": 1.0
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.380046,33.866132]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Constitution Lakes Park",
+			"number": 2,
+			"distance": 2.25
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.343247,33.681368]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Clyde Shepherd Nature Preserve",
+			"number": 3,
+			"distance": 1.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.284152,33.805975]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "South Peachtree Creek Trail",
+			"number": 4,
+			"distance": 3.25
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.296525,33.802700]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Hahn Woods & Lullwater Park",
+			"number": 5,
+			"distance": 2.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.323235,33.803836]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Herbert Taylor Park",
+			"number": 6,
+			"distance": 1.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.342486,33.798989]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Morningside Nature Preserve",
+			"number": 7,
+			"distance": 2.0
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.352171,33.809776]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "East Palisades",
+			"number": 8,
+			"distance": 4.25
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.441639,33.878001]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "West Palisades",
+			"number": 9,
+			"distance": 4.25
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.454363,33.871516]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Elwyn John Wildlife Sanctuary",
+			"number": 10,
+			"distance": 1.0
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.328021,33.824434]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Frazer Forest & Deep Dene Park",
+			"number": 11,
+			"distance": 2.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.326528,33.770880]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Glenlake Park & Decatur Cemetery",
+			"number": 12,
+			"distance": 1.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.290781,33.783583]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Piedmont Park & Eastside Beltline Trail",
+			"number": 13,
+			"distance": 2.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.368665,33.782074]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Tanyard Creek Park and Northside BeltLine Trail",
+			"number": 14,
+			"distance": 2.25
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.402675,33.810443]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Murphey Candler Park",
+			"number": 15,
+			"distance": 1.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.326311,33.909468]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Outdoor Activity Center",
+			"number": 16,
+			"distance": 1.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.434663,33.72889]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Cascade Springs Nature Preserve",
+			"number": 17,
+			"distance": 2.0
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.480699,33.719491]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "W.D. Thompson Park",
+			"number": 18,
+			"distance": 1.0
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.316964,33.808481]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Atlanta Memorial Park",
+			"number": 19,
+			"distance": 2.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.411257,33.818253]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Mercer University Nature Trail",
+			"number": 20,
+			"distance": 1.0
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.259926,33.871952]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Boundary Waters Park",
+			"number": 21,
+			"distance": 7.0
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.664472,33.667348]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Sweetwater Creek State Park",
+			"number": "22 & 23",
+			"distance": "3.0 & 3.5"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.628533,33.753329]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Clinton Nature Preserve",
+			"number": 24,
+			"distance": 3.75
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.861236,33.707948]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Heritage Park",
+			"number": 25,
+			"distance": 3.4
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.542493,33.839383]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Melvin L. Newman Wetlands Center",
+			"number": 26,
+			"distance": 0.75
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.305266,33.471928]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Cochran Mill Park",
+			"number": 27,
+			"distance": 5.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.715598,33.573974]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Cochran Mill Nature Center",
+			"number": 28,
+			"distance": 3.75
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.713516,33.580821]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Reynolds Nature Preserve",
+			"number": 29,
+			"distance": 2.25
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.346638,33.600881]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Line Creek Nature Area",
+			"number": 30,
+			"distance": 2.65
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.604983,33.391463]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Henderson Park",
+			"number": 31,
+			"distance": 2.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.232311,33.863313]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Davidson-Arabia Mountain Nature Preserve",
+			"number": "32-36",
+			"distance": "1.0 & 2.0 & 2.5 & 2.5 & 3.5"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.116353,33.671821]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Stone Mountain Park",
+			"number": "37-39",
+			"distance": "2.0 & 6.0 & 1.5"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.162063,33.810553]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Yellow River Park",
+			"number": "40 & 41",
+			"distance": "4.25 & 2.75"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.07073,33.789848]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Panola Mountain State Park",
+			"number": 42,
+			"note": "Summit only accessible via ranger-led hikes",
+			"distance": 2.0
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.171379,33.626253]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Kennesaw Mountain National Battlefield Park",
+			"number": "43-45",
+			"distance": "6.0 & 3.75 & 6.0"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.578462,33.975574]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Olde Rope Mill Park",
+			"number": 46,
+			"distance": 12.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.52348,34.131558]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Autrey Mill Nature Preserve & Heritage Center",
+			"number": 47,
+			"distance": 1.0
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.232628,34.020928]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Simpsonwood Conference & Retreat Center",
+			"number": 48,
+			"distance": 2.0
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.242723,33.986757]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "McDaniel Farm Park",
+			"number": 49,
+			"distance": 2.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.128272,33.968426]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Leita Thompson Memorial Park",
+			"number": 50,
+			"distance": 2.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.406529,34.063144]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Big Trees Forest Preserve",
+			"number": 51,
+			"distance": 1.25
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.361669,33.964347]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Dunwoody Nature Center",
+			"number": 52,
+			"distance": 1.25
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.333447,33.956751]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Cochran Shoals",
+			"number": 53,
+			"distance": 7.0
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.444024,33.902026]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Sope Creek",
+			"number": 54,
+			"distance": 4.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.442828,33.936898]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Johnson Ferry South",
+			"number": 55,
+			"distance": 1.6
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.408029,33.943846]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Johnson Ferry North",
+			"number": 56,
+			"distance": 2.0
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.406044,33.945747]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Gold Branch",
+			"number": 57,
+			"distance": 3.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.385261,33.984611]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Vickery Creek",
+			"number": 58,
+			"distance": 4.5
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.350218,34.006687]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Island Ford",
+			"number": 59,
+			"distance": 6.25
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.325948,33.986552]
+		}
+	},{
+		"type": "Feature",
+		"properties": {
+			"name": "Jones Bridge",
+			"number": 60,
+			"distance": 5.25
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-84.239447,34.000953]
+		}
+	}
+	]
+}
